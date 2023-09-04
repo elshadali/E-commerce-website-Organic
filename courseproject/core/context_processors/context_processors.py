94f6721d494c3ac52bd6f03a58bc826170e2bff5
elshad <elshadalizade111@gmail.com>
2023-09-04 20:28:48 +0400

    bugfix

--- courseproject/core/context_processors/context_processors.py
+++ courseproject/core/context_processors/context_processors.py
@@ -14,8 +14,8 @@ def header_and_footer(request):
     
     if request.user.is_authenticated:
         context.update({
-            'basket_order_count': OrderItem.objects.filter(user=request.user, status=0).count(),
-            'wish_list_count': WishList.objects.filter(user=request.user).first().product.count()
+            'basket_order_count': OrderItem.objects.filter(user=request.user, status=0).count() or 0,
+            'wish_list_count': WishList.objects.filter(user=request.user).first().product.count() if WishList.objects.filter(user=request.user).first() else 0
             })
 
 
