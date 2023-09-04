71207f99f1d80c8830d394626f3d51f1f8f7f592
elshad <elshadalizade111@gmail.com>
2023-09-04 20:03:16 +0400

    nginx.conf file name fixed

--- courseproject/courseproject/settings.py
+++ courseproject/courseproject/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jxkj(j@bq+6vo%!m@8x0@b&7bm=v1xywyd6ojq+n#m2-yv!o3('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False) != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
