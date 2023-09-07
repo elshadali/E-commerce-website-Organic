da88bc0838ed152553463a9fb9112aff2bbd0157
elshad <elshadalizade111@gmail.com>
2023-09-07 18:58:44 +0400

    bugfix

--- courseproject/static/apps/order/checkout.js
+++ courseproject/static/apps/order/checkout.js
@@ -17,3 +17,57 @@ function isDoneMainOrder() {
       }
     });
   }
+
+
+  function manageOrderButton() {
+    orderButton = document.getElementById('order-button');
+  
+    cardNumber = document.getElementById('card-number').value;
+    cardHolder = document.getElementById('card-holder').value;
+    expireDate = document.getElementById('expire-date').value;
+    cvv = document.getElementById('cvv').value;
+  
+    if (
+      cardNumber &&
+      cardHolder &&
+      expireDate &&
+      cvv
+    ) {
+      orderButton.disabled=false;
+    } else {
+      orderButton.disabled=true;
+    }
+  
+  }
+  
+  
+  function checkValidCardNumber() {
+    cardNumber = document.getElementById('card-number');
+    cardNumberError = document.getElementById('card-number-error');
+  
+    if (cardNumber.value.length !== 16) {
+      cardNumberError.classList.remove('d-none');
+    } else {
+      cardNumberError.classList.add('d-none');
+    }
+    if (cardNumber.value.length > 16) {
+      cardNumber.value = cardNumber.value.slice(0, 16);
+      checkValidCardNumber();
+    }
+  }
+  
+  
+  function checkValidCvv() {
+    cvv = document.getElementById('cvv');
+    cvvError = document.getElementById('cvv-error');
+  
+    if (cvv.value.length !== 3) {
+      cvvError.classList.remove('d-none');
+    } else {
+      cvvError.classList.add('d-none');
+    }
+    if (cvv.value.length > 3) {
+      cvv.value = cvv.value.slice(0, 3);
+      checkValidCvv();
+    }
+  }
