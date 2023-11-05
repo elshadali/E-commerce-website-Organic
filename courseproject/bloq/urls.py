1486de961eba12ce34f57c04749015d51354cfb7
elshad <elshadalizade111@gmail.com>
2023-11-05 16:07:19 +0400

    fix bloq_list

--- courseproject/bloq/urls.py
+++ courseproject/bloq/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BloqsListView
+from .views import BloqsListView, bloq_detail
 
 
 urlpatterns = [
-    path('bloqs/', BloqsListView.as_view(), name='bloq')
+    path('bloqs/', BloqsListView.as_view(), name='bloq'),
+    # path('bloqs/<slug:slug>', BloqDetailView.as_view(), name='bloq_detail'),
+    path('bloqs/<slug:bloq_slug>', bloq_detail, name='bloq_detail')
 ]
--- courseproject/bloq/views.py
+++ courseproject/bloq/views.py
@@ -1,25 +1,28 @@
+from typing import Any
 from django.shortcuts import render
 from django.views import generic
 from .models import Bloq, Author, Category
 
 
-def bloqs(request):
+def bloq_detail(request, bloq_slug):
 
-    bloq = Bloq.objects.all()
+    bloq = Bloq.objects.get(slug=bloq_slug)
     author = Author.objects.all()
     category = Category.objects.all()
+    blog_categories_ids = [cat.id for cat in bloq.category.all()]
+    relate_blogs = Bloq.objects.filter(category__in=blog_categories_ids).exclude(id=bloq.id).distinct()
 
     context = {
-        'bloqs' : bloq,
+        'bloq' : bloq,
         'authors' : author,
-        'categories' : category
+        'categories' : category,
+        'relate_blogs': relate_blogs
     }
 
-    return render(request, 'bloq/bloq.html', context)
-
+    return render(request, 'bloq/bloq_detail.html', context)
 
 class BloqsListView(generic.ListView):
     template_name = 'bloq/bloq.html'
     model = Bloq
     context_object_name = 'bloqs'
-    paginate_by = 1
+    paginate_by = 2
